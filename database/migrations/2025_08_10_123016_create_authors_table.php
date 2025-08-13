2e58972f43c06d5dd6f8541a3bb65e4bd3bfa0bd
suhaib-dwk <suhaibdwweekat1@gmail.com>
2025-08-13 15:10:55 +0300

    update error in seeds and factory

--- database/migrations/2025_08_10_123016_create_authors_table.php
+++ database/migrations/2025_08_10_123016_create_authors_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
              $table->id();
         $table->string('code')->unique();
         $table->string('name');
+        $table->string('email')->unique()->nullable();
+        $table->string('password')->nullable();
+        $table->string('phone')->nullable();
         $table->string('country');
         $table->integer('books_count')->default(0);
         $table->timestamps();
